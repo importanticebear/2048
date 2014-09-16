98f9f8f3f2ec0939d368e4f1f91a129c92254507
Jason Conner <jason.r.conner@gmail.com>
2014-09-15 21:47:07 -0500

    Replaced table with divs
    Removed the table in favor of using divs. I refactored some of the JS to
    work with the divs instead of the tds they had been working with but a
    good amount of the JS is still targeted at tds.
    
    I've got numbers randomly spawning and the distribution of 2s and 4s
    seems good to me. The problem is that the random spawner occasionally
    spawns a number in an occupied cell. This doesn't put two numbers in the
    cell but it overwrites the contents of the cell. This can be seen by
    constantly refreshing the page until the board spawns with only one
    starting tile. It's possible this is caused by an array index problem
    instead of randomly choosing the same cell but I doubt it.

--- Resources/2048.js
+++ Resources/2048.js
@@ -1,28 +1,8 @@
 function Board() {
+    this.emptyCell = ""; // Value to put in an empty cell
+    this.gridSquare = 4; // Number of cells on a side
+    this.piece = "<div id=piece>{0}</div>"
     this.twoFourRatio = 0.8; // 80% of new tiles are 2s
-    this.gridSquare = 4;
-    this.grid = $("td").toArray(); // Collection of all cells on the game board
-    this.gridKeys = [
-        "#row1col1",
-        "#row1col2",
-        "#row1col3",
-        "#row1col4",
-
-        "#row2col1",
-        "#row2col2",
-        "#row2col3",
-        "#row2col4",
-
-        "#row3col1",
-        "#row3col2",
-        "#row3col3",
-        "#row3col4",
-
-        "#row4col1",
-        "#row4col2",
-        "#row4col3",
-        "#row4col4"
-    ];
 }
 
 Board.prototype.BuildBoard = function() {
@@ -43,7 +23,7 @@ Board.prototype.CellAToB = function(indexA, indexB) {
  */
 Board.prototype.EmptyAllCells = function() {
     for(gridKey in this.gridKeys) {
-        this.ReplaceCellContents(gridKey, "");
+        this.ReplaceCellContents(gridKey, this.emptyCell);
     }
 }
 
@@ -61,6 +41,16 @@ Board.prototype.EmptyCellContents = function(cellIndex) {
  */
 Board.prototype.GetCellContents = function(cellIndex) {
     return $(this.gridKeys[cellIndex]).contents();
+}
+
+Board.prototype.GetEmptyCells = function() {
+    return $("div.cell:contains('')");
+}
+
+Board.prototype.GetRandomEmptyCell = function() {
+    var emptyCells = this.GetEmptyCells();
+    var index = this.RandomZeroToNExclusive(emptyCells.size());
+    return emptyCells[index];
 }
 
 /*
@@ -81,7 +71,7 @@ Board.prototype.NextNumber = function() {
  * the index for grid to choose a random cell.
  */
 Board.prototype.RandomCell = function() {
-    return Math.floor(Math.random() * Math.pow(this.gridSquare, 2));
+    return this.RandomZeroToNExclusive(Math.pow(this.gridSquare, 2));
 }
 
 /*
@@ -96,15 +86,26 @@ Board.prototype.RandomCellExcluding = function(excluding) {
     return cell;
 }
 
+Board.prototype.RandomSpawn = function() {
+    this.GetRandomEmptyCell().innerText = this.NextNumber();
+}
+
 /*
  * Randomly selects two cells on the game board and spawns either a 2 or 4
  * in them based on the twoFourRatio.
  */
 Board.prototype.RandomStart = function() {
-    var cell1 = this.RandomCell();
-    var cell2 = this.RandomCellExcluding(cell1);
-    var num1 = this.NextNumber();
-    var num2 = this.NextNumber();
+    this.RandomSpawn();
+    this.RandomSpawn();
+}
+
+/*
+ * Select a random number from 0 to n, exclusive. For example, an n of 16
+ * will select from each whole number starting at and including 0 and going
+ * up to and including 15.
+ */
+Board.prototype.RandomZeroToNExclusive = function(n) {
+    return Math.floor(Math.random() * n);
 }
 
 /*
@@ -113,7 +114,15 @@ Board.prototype.RandomStart = function() {
  */
 Board.prototype.ReplaceCellContents = function(cellIndex, contents) {
     this.EmptyCellContents(cellIndex);
-    $(this.gridKeys[cellIndex]).append(contents);
+    $(this.gridKeys[cellIndex]).append(this.WrapContentsInDiv(contents));
+}
+
+Board.prototype.WrapContentsInDiv = function(contents) {
+    var div = "";
+    if (contents !== "") {
+        div = this.piece.format(contents);
+    }
+    return div;
 }
 
 /*
@@ -139,4 +148,18 @@ $(document).keydown(function(event) {
 $(document).ready(function() {
     var board = new Board();
     board.EmptyAllCells();
+    board.RandomStart();
 });
+
+/*
+ * Found this method here: http://stackoverflow.com/questions/18405736/is-there-a-c-sharp-string-format-equivalent-in-javascript
+ * It essentially behaves like C#'s string.Format().
+ */
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+  };
+}
